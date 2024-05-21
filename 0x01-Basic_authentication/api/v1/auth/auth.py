8b2d176555ac5e03ecf564b0b475d2e9706303de
rs412-wsl_ubuntu_18.04LTS <oussag@hotmail.fr>
2024-05-21 13:09:49 +0100

    TASK 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -38,6 +38,13 @@ class Auth:
         if request is None:
             return None
 
+        header = request.headers.get('Authorization')
+
+        if header is None:
+            return None
+
+        return header
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         current user
