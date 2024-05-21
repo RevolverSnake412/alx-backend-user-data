68466d7d0a74f5521901c59bba3fdb496ab9d4ba
rs412-wsl_ubuntu_18.04LTS <oussag@hotmail.fr>
2024-05-21 14:41:45 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 """
-auth
+auth.py
 """
+
+
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
+    """
+    Auth class
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         require auth
